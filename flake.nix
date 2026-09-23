610dc79331c10241c45ff8978643059a6b19ec78
Asadlee24 <asadlee24@users.noreply.github.com>
2026-09-23 12:26:00 +0500

    fix: point atlasmirror_sdk input to GitHub repo

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder/0c5b062fd11b20f85cc7c0720ddcac1cbbb46c4c";
-    atlasmirror_sdk.url = "path:../atlasmirror-sdk";
+    atlasmirror_sdk.url = "github:Asadlee24/atlasmirror-sdk";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
